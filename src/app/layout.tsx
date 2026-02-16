c0595242c667c258f008026f4b8f112f9f4091a6
J0T0S0 <tosato.giovanni@gmail.com>
2026-02-16 21:31:45 +0100

    reverted some dependency versions to match MineStore's

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,28 +2,15 @@ import './globals.css';
 import { getEndpoints } from '@/api';
 import { fetcher } from '@/api/server/fetcher';
 
-import type { Metadata, Viewport } from 'next';
+import type { Metadata } from 'next';
 import { App } from './app';
 import 'react-quill-new/dist/quill.snow.css';
-import { readFileSync } from 'fs';
-import { extractConfigValue } from '@helpers/extract-config-value';
-import { Config } from '@/app/providers/config-provider';
 
 const { getSettings } = getEndpoints(fetcher);
 
-const configFile = readFileSync(process.cwd() + '/config.json', 'utf8');
-const configData = JSON.parse(configFile) as Config;
-const themeColor = extractConfigValue('main-color', configData) || '#2B2B2BFF';
-
-export function generateViewport(): Viewport {
-    return {
-        themeColor
-    };
-}
-
 export const generateMetadata = async (): Promise<Metadata> => {
     try {
-        const { website_name, website_description } = await getSettings();
+        const { website_description } = await getSettings();
 
         return {
             title: 'Quantum Craft',
